6956e2f2a706bdabb479a7540a03150ac1092083
Amimul <1805056@ugrad.cse.buet.ac.bd>
2021-06-17 22:05:55 +0600

    File reading Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) throws FileNotFoundException {
+
+        File file = new File("input.txt");
+        Scanner scanner = new Scanner(file);
+
+        int n = scanner.nextInt();
+
+        int[] arrX = new int[n];
+        int[] arrY = new int[n];
+
+        int i = 0;
+
+        while (scanner.hasNextLine()) {
+            arrX[i] = scanner.nextInt();
+            arrY[i] = scanner.nextInt();
+            i++;
+        }
+
 
     }
 }
